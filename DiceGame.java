a65adadfd15b2f0cc3d5c539f8cb39a16d592c27
Hikaru Kakizawa <h.k.917_gk@me.com>
2019-07-05 13:43:31 +0900

    Initial revision

---
+++ DiceGame.java
@@ -0,0 +1,35 @@
+import java.util.Random;
+
+class Die {
+  private int faceValue;
+
+  public int getFaceValue(){
+    return faceValue;
+  }
+  public void roll(){
+    faceValue = (int)(Math.random()*5) + 1;
+  }
+}
+
+public class DiceGame {
+  Die die1 = new Die();
+  Die die2 = new Die();
+
+  public void play(){
+    int fv1;
+    int fv2;
+    die1.roll();
+    fv1 = die1.getFaceValue();
+    die2.roll();
+    fv2 = die2.getFaceValue();
+    System.out.println("Rolling the dice...");
+    System.out.println("Die 1: "+fv1);
+    System.out.println("Die 2: "+fv2);
+    System.out.println("Total value: "+(fv1 + fv2));
+  }
+
+  public static void main(String[] args){
+    DiceGame dice = new DiceGame();
+    dice.play();
+  }
+}
